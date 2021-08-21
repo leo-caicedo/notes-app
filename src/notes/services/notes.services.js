bc81a0f70615422487188a13007074aee96fb4c3
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-08-21 14:45:33 -0500

    createNote

--- src/notes/services/notes.services.js
+++ src/notes/services/notes.services.js
@@ -5,7 +5,20 @@ class NotesServices {
   // list notes
   async getNotes(req, res) {
     const notes = await Note.find({}).lean();
-    res.render("notes/home");
+    res.render("notes/notes", { notes });
+  }
+
+  // create note
+  renderNoteForm(req, res) {
+    res.render("notes/new-note");
+  }
+  async createNote(req, res) {
+    const { body: note } = req;
+
+    const noteCreated = new Note(note);
+    await noteCreated.save();
+    req.flash("success_msg", "Note added successfully");
+    res.redirect("/notes");
   }
 }
 
